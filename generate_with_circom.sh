3234f0abf504bc5728ca8a441b318608ed685939
Sun Jia Cheng <sunjc826@gmail.com>
2024-03-10 11:28:31 +0800

    Move scripts

--- generate_with_circom.sh
+++ generate_with_circom.sh
@@ -3,6 +3,12 @@ read -r CIRCOM_FILE_BASENAME
 : "${CIRCOM_FILE_BASENAME:="aux_vector_clock"}"
 set -e
 pushd src/circom_circuits
+if ! command -v circom > /dev/null
+then
+    echo "circom isn't installed"
+    echo "Please install circom manually and add to PATH" 
+    exit 1
+fi
 circom --r1cs --sym --c --O2 -- "$CIRCOM_FILE_BASENAME".circom
 ln -s ../../../json/include/nlohmann "$CIRCOM_FILE_BASENAME"_cpp
 pushd "$CIRCOM_FILE_BASENAME"_cpp
